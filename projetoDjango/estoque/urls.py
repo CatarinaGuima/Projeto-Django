fbf961b48171c6c5e738e517f944ec0b8eab50a8
Catarina Guimarães <110750750+CatarinaGuima@users.noreply.github.com>
2023-06-25 00:54:29 -0300

    added estoque commit

--- projetoDjango/estoque/urls.py
+++ projetoDjango/estoque/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('estoque/', views.estoque, name="estoque"), 
+    path('estoque/', views.view_registrar_estoque, name="estoque"), 
+    path('estoque/busca/<id>', views.view_buscar_estoque, name="buscar_estoque"),
 ]
--- projetoDjango/estoque/views.py
+++ projetoDjango/estoque/views.py
@@ -1,6 +1,66 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
+import json
+from database.models import Estoque #nome da classe do models de database
 
 # Create your views here.
-def estoque(request):
-    return HttpResponse("Bem-vindo(a) à página de estoque")
+def parse_json(query_serializer):
+    resposta_json = []
+    for item in query_serializer:
+        itens = {
+                  "setor": item['fields']['setor'],
+                  "corredor": item['fields']['corredor'],
+                  "prateleira": item['fields']['prateleira'],
+                  "nome_produto": item['fields']['nome_produto'],
+                }
+        resposta_json.append(itens)
+    return resposta_json    
+
+def view_registrar_estoque(request):
+    if request.method == 'POST':
+        decode_json = request.body.decode('utf-8')
+        registro_estoque= json.loads(decode_json)
+        banco = Estoque(setor=registro_estoque['setor'], corredor=registro_estoque['corredor'], prateleira=registro_estoque['prateleira'], nome_produto=registro_estoque['nome_produto'])
+        banco.save()
+        return JsonResponse({
+            "status": "Cadastro Realizado com Sucesso",
+            "registro": {
+                "id": banco.pk,
+                "setor":  registro_estoque['setor'],
+                "corredor": registro_estoque['corredor'],
+                "prateleira": registro_estoque['prateleira'],
+                "nome_produto": registro_estoque['nome_produto']
+            }
+        })
+    elif request.method == 'GET':
+        query_set = Estoque.objects.all()
+        query_serializer = serializers.serialize('json', query_set)
+        nomes_json = json.loads(query_serializer)
+        resposta_json = parse_json(nomes_json)
+        return JsonResponse(resposta_json, safe=False)    
+    elif request.method == 'PUT':
+        decode_json = request.body.decode('utf-8')
+        registro_estoque = json.loads(decode_json)
+        query_set = Estoque.objects.get(pk=registro_estoque['id'])
+        query_set.setor = registro_estoque['setor']
+        query_set.corredor = registro_estoque['corredor']
+        query_set.prateleira = registro_estoque['prateleira']
+        query_set.nome_produto = registro_estoque['nome_produto']
+        query_set.save()
+        return JsonResponse({
+            "status": "Cadastro Atualizado com sucesso.",
+            "registro": {
+                "id": query_set.pk,
+                "setor": query_set.setor,
+                "corredor": query_set.corredor,
+                "prateleira": query_set.prateleira,
+                "nome_produto": query_set.nome_produto,
+            }
+        })
+
+def view_buscar_estoque(request, id):
+    if request.method == 'GET':
+        query_set = Estoque.objects.get(pk=id)
+        query_serialize = json.loads(serializers.serialize('json', [query_set]))
+        resposta_json = parse_json(query_serialize)                                     
+    return JsonResponse(resposta_json, safe=False)

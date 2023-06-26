e790d5903e03023bc62cca5c7a52389f75c95b4c
Catarina Guimarães <110750750+CatarinaGuima@users.noreply.github.com>
2023-06-26 19:30:13 -0300

    added login database

--- projetoDjango/login/views.py
+++ projetoDjango/login/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from database.models import Login
+from django.core import serializers
+import json
 
-
+# Create your views here.
 def login(request):
     if request.method == 'POST':
         # Credenciais do usuário enviadas na solicitação
@@ -22,6 +24,64 @@ def login(request):
             return JsonResponse({'message': 'Login bem-sucedido'})
         else:
             # Credenciais inválidas
-            return JsonResponse({'message': 'Credenciais negadas'}, status=401)
+            return JsonResponse({'message': 'Login inválido'}, status=401)
     
     return render(request, 'login/login.html')
+
+def parse_json(query_serializer):
+    resposta_json = []
+    for item in query_serializer:
+        itens = {
+                 "username": item['fields']['username'],
+                 "password": item['fields']['password'],
+                }
+        resposta_json.append(itens)
+    return resposta_json    
+
+def view_registrar_usuarios(request):
+    if request.method == 'POST':
+        decode_json = request.body.decode('utf-8')
+        registro_login = json.loads(decode_json)
+        banco = Login(username=registro_login['username'], password=registro_login['password'])
+        banco.save()
+        return JsonResponse({
+            "status": "Cadastro Realizado com Sucesso",
+            "registro": {
+                "id": banco.pk,
+                "username":  registro_login['username'],
+                "password": registro_login['password'],
+            }
+        })
+    elif request.method == 'GET':
+        query_set = Login.objects.all()
+        query_serializer = serializers.serialize('json', query_set)
+        nomes_json = json.loads(query_serializer)
+        resposta_json = parse_json(nomes_json)
+        return JsonResponse(resposta_json, safe=False)    
+    elif request.method == 'PUT':
+        decode_json = request.body.decode('utf-8')
+        registro_produto = json.loads(decode_json)
+        query_set = Login.objects.get(pk=registro_produto['id'])
+        query_set.username = registro_produto['username']
+        query_set.password = registro_produto['password']
+        query_set.save()
+        return JsonResponse({
+            "status": "Cadastro Atualizado com sucesso.",
+            "registro": {
+                "id": query_set.pk,
+                "username": query_set.username,
+                "password": query_set.password,
+            }
+        })
+    elif request.method == 'DELETE':
+        decode_json = request.body.decode('utf-8')
+        registro_login = json.loads(decode_json)
+        query_set = Login.objects.get(pk=registro_login['id'])
+        query_set.delete()
+
+def view_buscar_usuarios(request, id):
+    if request.method == 'GET':
+        query_set = Login.objects.get(pk=id)
+        query_serialize = json.loads(serializers.serialize('json', [query_set]))
+        resposta_json = parse_json(query_serialize)                                     
+    return JsonResponse(resposta_json, safe=False)
